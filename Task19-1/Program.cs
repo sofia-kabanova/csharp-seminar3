e7fbe1a7a26f0a500bff24e29b52bdddf77a02da
Sofia Kabanova <sofia.kabanova@gmail.com>
2022-09-28 01:12:22 +0300

    done task19-1

--- Task19-1/Program.cs
+++ Task19-1/Program.cs
@@ -1,15 +1,29 @@
 ﻿
-  // 19. Напишите программу, которая 
+// 19. Напишите программу, которая 
 // принимает на вход число
 // переворачивает его
 Console.Clear();
 Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 Console.Write($" {num} --> ");
+int num1 = num % 10;
+int num2 = ((num/10) %10);
+int num3 = ((num/100) %10);
+int num4 = ((num/1000) %10);
+Console.Write(num1);
+Console.Write(num2);
+Console.Write(num3);
+Console.Write(num4);
 
-    while (num % 10 > 0)
-    {
-        Console.Write(num % 10);
-        num -- ;
-    }
 
+
+    //while (num1 % 10 < 0)
+    //{
+      //  num1=num1 % 10;
+        //num1=num1/10;
+       // num1--;  
+       // Console.Write(num1);
+   // }
+
+//if (num == num1) Console.Write($"{num} Да");
+//else Console.Write("Нет");
---
+++ Task19-2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write($" {num} --> ");
+while ( num % 10 > 0 )
+{
+   Console.Write(num % 10);
+   num = num/10;
+} 
